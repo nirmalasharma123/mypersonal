033b08bf98547bb2dd93a4527091a4e6afa05c6b
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-01-06 11:36:15 +0530

    Update authourController.js

--- bloggingsite/src/controllers/authourController.js
+++ bloggingsite/src/controllers/authourController.js
@@ -5,23 +5,30 @@ const author=require("../modules/authorModel");
 const createAuthor=async function(req,res){
   try {
     let data=req.body;
+   
+    if(!data.fname||!data.lname||!data.title||!data.email||!data.password) return res.status(400).send({status:false,msg:"please fill all the fields"})
 
-    if(!(validator.isAlpha(data.fname)||validator.isAlpha(data.lname)))  return res.status(400).send("invalid name")
+    let fname=data.fname.trim().split(" ").join("")
+    
+    let lname=data.lname.trim().split(" ").join("")
+   data.fname=fname;
+   data.lname=lname;
+
+    if(!validator.isAlpha(data.fname)||!validator.isAlpha(data.lname))  return res.status(400).send({status:false,msg:"invalid name"})
      
     
-    let fname=data.fname.trim().split(" ").join("")
-     let lname=data.lname.trim().split(" ").join(""); 
-    data.fname=fname
-    data.lname=lname;
+    
 
-     if(!(validator.isEmail(req.body.email))) return res.status(400).send("please put a valid email")
+     if(!(validator.isEmail(req.body.email))) return res.status(400).send({status:false,result:"please put a valid email"})
      
      if (!validator.isStrongPassword(data.password)) {
       return res.status(400).send({ status: false, msg: "Kindly use atleast one uppercase alphabets, numbers and special characters for strong password." })
 }
+let checkEmail=await authorModel.findOne({email:data.email})
+if(checkEmail) return res.status(400).send({status:false,result:"email already exists"})
      
      let setData=await author.create(data);
-    res.status(201).send({status:true,msg:setData});
+    res.status(201).send({status:true,data:setData});
 }catch(error){
     res.status(500).send({status:false,error:error.message})
 }
